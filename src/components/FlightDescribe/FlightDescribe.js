f5ce238075f3b1fc5b31b424c2e63f7c60a4f3db
Julia <yulya_9060@mail.ru>
2018-12-04 22:35:48 +0300

    добавила задержку рейсов

--- src/components/FlightDescribe/FlightDescribe.js
+++ src/components/FlightDescribe/FlightDescribe.js
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './FlightDescribe.scss';
 
-const FlightDescribe = ({ arrival, departure, duration }) => {
+const FlightDescribe = ({
+    arrival, departure, duration, startDate,
+}) => {
     let describe = '';
-    const arrivalDate = new Date(arrival);
-    const departureDate = new Date(departure);
+    let arrivalDate = `${startDate}T${arrival}`;
+    arrivalDate = +new Date(arrivalDate);
+    let departureDate = `${startDate}T${departure}`;
+    departureDate = +new Date(departureDate);
     if (departureDate - arrivalDate > duration) {
         describe = 'Рейс задержан';
-    } else if (new Date() < arrivalDate && new Date() > departureDate) {
-        describe = 'В воздухе';
-    } else if (new Date() > arrivalDate) {
+    } else if (+new Date() > arrivalDate) {
         describe = 'Рейс прибыл';
     }
     return <div className="flightDescribe">{describe}</div>;
@@ -20,12 +22,14 @@ FlightDescribe.propTypes = {
     arrival: PropTypes.string,
     departure: PropTypes.string,
     duration: PropTypes.number,
+    startDate: PropTypes.string,
 };
 
 FlightDescribe.defaultProps = {
     arrival: '',
     departure: '',
     duration: 0,
+    startDate: '',
 };
 
 export default FlightDescribe;
